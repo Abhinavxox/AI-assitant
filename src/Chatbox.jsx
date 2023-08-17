81eafb77b071149677a139ae3bcf62e6a83df630
Abhinavxox <abhinavxox@gmail.com>
2023-08-17 23:30:59 +0545

    Done

--- src/Chatbox.jsx
+++ src/Chatbox.jsx
@@ -1,16 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
 const Chatbox = () => {
   const [messages, setMessages] = useState([]);
+  const inputRef = useRef(null);
 
   const handleMessageSend = () => {
-    const newMessages = [...messages, { text: "hi", sender: "bot" }];
+    const userMessage = inputRef.current.value;
+    const newMessages = [
+      ...messages,
+      { text: userMessage, sender: "user" },
+      { text: "Hi", sender: "bot" }, // Bot's response
+    ];
     setMessages(newMessages);
+    inputRef.current.value = "";
   };
 
+  // Automatically scroll to the bottom of the messages when a new message is added.
+  useEffect(() => {
+    if (messages.length > 0) {
+      const messageContainer = document.getElementById("message-container");
+      messageContainer.scrollTop = messageContainer.scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <div className="flex flex-col">
-      <div className="flex-grow bg-gray-100 p-4">
+    <div className="flex flex-col h-full">
+      <div
+        id="message-container"
+        className="flex-grow bg-gray-100 p-4 overflow-y-auto"
+      >
         <div className="space-y-4">
           {messages.map((message, index) => (
             <Message key={index} text={message.text} sender={message.sender} />
@@ -20,6 +38,7 @@ const Chatbox = () => {
       <div className="p-4">
         <div className="flex space-x-2">
           <input
+            ref={inputRef}
             type="text"
             placeholder="Type your message..."
             className="flex-grow border p-2 rounded"
@@ -42,8 +61,8 @@ const Message = ({ text, sender }) => {
   return (
     <div
       className={`p-2 rounded ${
-        isBot ? "bg-gray-200" : "bg-blue-500 text-white"
-      } ${isBot ? "ml-2" : "mr-2"}`}
+        isBot ? "bg-gray-200 ml-2" : "bg-blue-500 text-white mr-2"
+      }`}
     >
       {text}
     </div>
